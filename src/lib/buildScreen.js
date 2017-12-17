da2e2b7ad3e6efd933308e6d5f5b8ebff80585d4
Zachary Marois <zachary.marois@gmail.com>
2017-12-17 15:05:48 -0500

    Better screen that chooses decisively

--- src/lib/buildScreen.js
+++ src/lib/buildScreen.js
@@ -3,15 +3,13 @@ var fs = require('fs'),
     Promise = require('bluebird'),
     pixelmatch = require('pixelmatch');
 
+// Takes an images that is greyscale and red and converts it to black and white.
 var decide = function(image) {
+    image.opaque();
     image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
-        
-            var red   = this.bitmap.data[ idx + 0 ];
-            var green = this.bitmap.data[ idx + 1 ];
-            var blue  = this.bitmap.data[ idx + 2 ];
-            var alpha = this.bitmap.data[ idx + 3 ];
-        
-        });
+        this.bitmap.data[idx] = this.bitmap.data[idx + 1] = this.bitmap.data[idx + 2] = 
+            this.bitmap.data[idx] == 255 ? 255 : 0;
+    });
 };
 
 if (require.main === module) {
@@ -20,8 +18,7 @@ if (require.main === module) {
     prompt.start();
     var sourceFilename = process.argv[2] || (await Promise.promisify(prompt.get)(['sourceFilename1'])).sourceFilename;
     let image = await Promise.promisify(jimp.read)(sourceFilename);
-    image.posterize(2);
-    image.opaque();
+    decide(image);
     image.write("screen.png");
     }();
 }
